76f70830553de0e4487a77d0d6b61c05fe69aefe
viiku <vivekvikkusingh@gmail.com>
2024-08-18 00:32:52 +0530

    adding serializer

---
+++ todobackend/api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from todo.models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    # auto populated by app. User can’t manipulate
+    created = serializers.ReadOnlyField()
+    completed = serializers.ReadOnlyField()
+    
+    class Meta:
+        model = Todo
+        fields = ['id','title','memo','created','completed']
+        
