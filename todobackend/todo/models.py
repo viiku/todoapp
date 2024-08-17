fdc232b693a3cdfdde142dbddc19d880fd66444a
viiku <vivekvikkusingh@gmail.com>
2024-08-17 23:53:35 +0530

    add apps and backend framework

---
+++ todobackend/todo/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField(blank=True)
+    # set current time
+    created = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(blank=True)
+    # add which user has posted
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title 
+    
