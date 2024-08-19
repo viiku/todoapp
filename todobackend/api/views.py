a531f7c5b1c8a579df27f329284dad376c65b571
viiku <vivekvikkusingh@gmail.com>
2024-08-19 21:28:05 +0530

    added signup feature

--- todobackend/api/views.py
+++ todobackend/api/views.py
@@ -1,6 +1,12 @@
 from rest_framework import generics, permissions
 from .serializers import TodoSerializer, TodoToggleCompleteSerializer
 from todo.models import Todo
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
@@ -49,3 +55,19 @@ class TodoToggleComplete(generics.UpdateAPIView):
     def perform_update(self,serializer):
         serializer.instance.completed=not(serializer.instance.completed)
         serializer.save()
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request) # data is a dictionary
+            user = User.objects.create_user(
+                username = data['username'],
+                password = data['password']
+            )
+            user.save()
+            
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse({'error':'username token. choose another username'},status=400)
